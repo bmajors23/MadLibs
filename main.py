95167fa1852baccbd47f1f90009d357c084d2447
bmajors23 <42188113+bmajors23@users.noreply.github.com>
2021-05-17 11:50:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Lets play mad-libs")
+
+noun1 = input("Give me a noun: ")
+noun2 = input("Give me a plural noun: ")
+verb1 = input("Give me a verb: ")
+adjective1 = input("Give me an adjective: ")
+noun3 = input("Give me the name of a place: ")
+adjective2 = input("Give me an adjective: ")
+verb2 = input("Give me a verb: ")
+noun4 = input("Give me a noun: ")
+adjective3 = input("Give me an adjective: ")
+verb3 = input("Give me a verb ending in ed: ")
+
+print(f"There once was a {noun1} who liked to spend time with {noun2}. \n"
+      f"They would {verb1} all day long. After the long day of {verb1} \nthey would feel very {adjective1}. "
+      f"So they decided to go to the {noun3}.\n After going to the {noun3}, they would feel very {adjective2}.\n"
+      f"So then they {verb2} until they couldn't anymore.\n Finally, they decided to buy a {noun4}.\n"
+      f" Which turned the {noun1} into a {adjective3} pickle. \nSo the {noun1} {verb3}. The end.")
